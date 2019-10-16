a9f45a17e312c33a9047fc671a2810ff5cb65c85
layoomielie <layoomielie@gmail.com>
2019-10-16 21:18:59 +0800

    freemark

---
+++ thread-server/src/main/java/com/example/util/TokenProccessor.java
@@ -0,0 +1,47 @@
+package com.example.util;
+
+import sun.misc.BASE64Encoder;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+/**
+ * @author：张鸿建
+ * @time：2019/10/16 21:16
+ * @desc：
+ **/
+public class TokenProccessor {
+
+
+    private TokenProccessor(){};
+    private static final TokenProccessor instance = new TokenProccessor();
+
+    public static TokenProccessor getInstance() {
+        return instance;
+    }
+
+    /**
+     * 生成Token
+     * @return
+     */
+    public String makeToken() {
+        String token = (System.currentTimeMillis() + new Random().nextInt(999999999)) + "";
+        token="layoomielie";
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte md5[] =  md.digest(token.getBytes());
+            BASE64Encoder encoder = new BASE64Encoder();
+            return encoder.encode(md5);
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String s = new TokenProccessor().makeToken();
+        System.out.println(s);
+    }
+}
