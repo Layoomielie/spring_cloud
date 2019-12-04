3553b7cf12a2f7617c2b8479400cb3f1d83fca12
layoomielie <Layoomielie>
2019-12-05 00:09:40 +0800

    shiro2

---
+++ shiro2-server/src/main/java/com/example/Shiro2ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shiro2ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shiro2ServerApplication.class, args);
+    }
+
+}
